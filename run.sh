dbc5a3b97b9c4871ea999bf4b3a108c23b9f3920
Vladimir Voinea <voineavladimir@gmail.com>
2024-04-28 01:35:56 +0300

    multiboot header is found but the 4gib limit is surpassed

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-qemu-system-x86_64 -cdrom out/src/iso/kernel.iso
+qemu-system-x86_64 -cdrom out/src/iso/kernel.iso -serial stdio -m 1024M
--- src/kernel/src/kernel.cxx
+++ src/kernel/src/kernel.cxx
@@ -2,12 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <bootstrap.hpp>
- 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
-#if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
- 
 
 void foo(const char* str)
 {
@@ -15,7 +9,7 @@ void foo(const char* str)
 	memcpy(buffer, str, strlen(str));
 }
  
-extern "C" void kernel_main(void) 
+extern "C" void Kernel_Main(void) 
 {
 	printf("vmr kernel started\n");
 	printf("Long mode is %s\n", is_long_mode_available() ?  "available" : "not available");
